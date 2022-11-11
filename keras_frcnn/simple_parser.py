c5ff41071ccba6a7468de70902990ee5240e25c9
Nourhan Abosaeed <66885297+nourhan412@users.noreply.github.com>
2022-11-11 20:22:53 +0200

    Update simple_parser.py

--- keras_frcnn/simple_parser.py
+++ keras_frcnn/simple_parser.py
@@ -33,7 +33,6 @@ def get_data(input_path):
 			if filename not in all_imgs:
 				all_imgs[filename] = {}
 				img = cv2.imread(filename)
-				assert not isinstance(img,type(None))
 				(rows,cols) = img.shape[:2]
 				all_imgs[filename]['filepath'] = filename
 				all_imgs[filename]['width'] = cols
