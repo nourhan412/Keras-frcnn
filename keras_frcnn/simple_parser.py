d6be99b41dd8f83f89ff565280263c94c8b17393
Nourhan Abosaeed <66885297+nourhan412@users.noreply.github.com>
2022-11-11 20:17:10 +0200

    Update simple_parser.py

--- keras_frcnn/simple_parser.py
+++ keras_frcnn/simple_parser.py
@@ -17,7 +17,7 @@ def get_data(input_path):
 
 		for line in f:
 			line_split = line.strip().split(',')
-			(filename,x1,x2,y1,y2,class_name) = line_split
+			(filename,x1,y1,x2,y2,class_name) = line_split
 
 			if class_name not in classes_count:
 				classes_count[class_name] = 1
@@ -29,17 +29,19 @@ def get_data(input_path):
 					print('Found class name with special name bg. Will be treated as a background region (this is usually for hard negative mining).')
 					found_bg = True
 				class_mapping[class_name] = len(class_mapping)
+
 			if filename not in all_imgs:
 				all_imgs[filename] = {}
-				img = cv2.imread(filename)
 				assert not isinstance(img,type(None))
+				img = cv2.imread(filename)
 				(rows,cols) = img.shape[:2]
 				all_imgs[filename]['filepath'] = filename
 				all_imgs[filename]['width'] = cols
 				all_imgs[filename]['height'] = rows
 				all_imgs[filename]['bboxes'] = []
 				all_imgs[filename]['imageset'] = 'test'
-                         all_imgs[filename]['bboxes'].append({'class': class_name, 'x1': float(x1), 'x2': float(x2), 'y1': float(y1), 'y2': float(y2)})
+
+			all_imgs[filename]['bboxes'].append({'class': class_name, 'x1': float(x1), 'x2': float(x2), 'y1': float(y1), 'y2': float(y2)})
 
 
 		all_data = []
@@ -55,5 +57,4 @@ def get_data(input_path):
 				class_mapping[key_to_switch] = val_to_switch
 		
 		return all_data, classes_count, class_mapping
-
 
