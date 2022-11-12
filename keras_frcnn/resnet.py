9e56dcdb190164d8c62ac2bde67138bd065d36ad
Nourhan Abosaeed <66885297+nourhan412@users.noreply.github.com>
2022-11-12 18:03:38 +0200

    Update resnet.py

--- keras_frcnn/resnet.py
+++ keras_frcnn/resnet.py
@@ -157,9 +157,9 @@ def nn_base(input_tensor=None, trainable=False):
 
     # Determine proper input shape
     if K.common.image_dim_ordering() == 'th':
-        input_shape = (3,0, 0)
+        input_shape = (3,None, None)
     else:
-        input_shape = (0,0, 3)
+        input_shape = (None,None, 3)
 
     if input_tensor is None:
         img_input = Input(shape=input_shape)
